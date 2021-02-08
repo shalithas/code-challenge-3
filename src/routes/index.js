b3f97645e31c85607db724eb08b9fa1dbb232826
Faheem-BigStep <faheem.khan@bigsteptech.com>
2021-02-08 18:41:07 +0530

    can get token

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,30 @@
-import React from 'react';
-import Discover from './Discover';
+import React, { useEffect, useState } from "react";
+import config from "../config";
+import Discover from "./Discover";
 
 export default function Routes() {
+  const [token, setToken] = useState("");
+  const [error, setError] = useState(false);
+  // Get AccessToken
+  useEffect(() => {
+    const auth = btoa(`${config.api.clientId}:${config.api.clientSecret}`);
+    const body = new URLSearchParams();
+    body.append("grant_type", "client_credentials");
+    const getToken = async () => {
+      const res = await fetch(`${config.api.authUrl}`, {
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${auth}`,
+        },
+        body,
+        redirect: "follow",
+      });
+      const result = await res.json();
+      setToken(result.access_token);
+    };
+    getToken();
+  }, [error]);
+
   // Here you'd return an array of routes
   return <Discover />;
 }

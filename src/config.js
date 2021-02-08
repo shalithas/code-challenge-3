9a2a9075ef9c92792a19a908f0f9cbc4e6a2c80f
Faheem-BigStep <faheem.khan@bigsteptech.com>
2021-02-08 18:40:55 +0530

    added key and secreat

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
   api: {
-    baseUrl: 'https://api.spotify.com/v1',
-    authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
-  }
-}
+    baseUrl: "https://api.spotify.com/v1",
+    authUrl: "https://accounts.spotify.com/api/token",
+    clientId: "e3376c9cfc604ca29fb0255d41ed7c94",
+    clientSecret: "9fd657003b80432999e0a8d48d25425f",
+  },
+};

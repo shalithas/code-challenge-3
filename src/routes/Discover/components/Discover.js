ff0b26dc488f675ee37790453a487397ad56917c
Faheem-BigStep <faheem.khan@bigsteptech.com>
2021-02-08 18:53:26 +0530

    getting data from props insted of state

--- src/routes/Discover/components/Discover.js
+++ src/routes/Discover/components/Discover.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import React, { Component } from "react";
+import "../styles/_discover.scss";
+import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock";
 
 export default class Discover extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      newReleases: [],
-      playlists: [],
-      categories: []
-    };
-  }
-
   render() {
-    const { newReleases, playlists, categories } = this.state;
+    const { newReleases, playlists, categories } = this.props;
 
     return (
       <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
+        <DiscoverBlock
+          text="RELEASED THIS WEEK"
+          id="released"
+          data={newReleases}
+        />
+        <DiscoverBlock
+          text="FEATURED PLAYLISTS"
+          id="featured"
+          data={playlists}
+        />
+        <DiscoverBlock
+          text="BROWSE"
+          id="browse"
+          data={categories}
+          imagesKey="icons"
+        />
       </div>
     );
   }

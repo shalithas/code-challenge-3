c5f79a7a6dc5d17816370694ddffee963513c213
NagamaniMekala <nagamani@aapkapainter.com>
2022-04-15 16:27:40 +0530

    newRealse, playlist and categories api calls are added

--- src/routes/Discover/components/Discover.js
+++ src/routes/Discover/components/Discover.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
-
+import axios from "axios"
+import api from "../../../config"
+const spotify = api.api;
 export default class Discover extends Component {
   constructor() {
     super();
-
     this.state = {
       newReleases: [],
       playlists: [],
-      categories: []
+      categories: [],
     };
   }
+  componentDidMount = async () => {
+    await axios(`${spotify.authUrl}`, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(spotify.clientId + ':' + spotify.clientSecret)
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+      .then(tokenResponse => {
+        axios(`${spotify.baseUrl}/new-releases`, {
+          method: "GET",
+          headers: { 'Authorization': 'Bearer ' + tokenResponse.data.access_token }
 
+        })
+          .then((newReleaseResponse) => {
+            this.setState({ newReleases: newReleaseResponse.data.albums.items })
+          })
+        axios(`${spotify.baseUrl}/featured-playlists`, {
+          method: "GET",
+          headers: { 'Authorization': 'Bearer ' + tokenResponse.data.access_token }
+
+        })
+          .then((featuredListResponse) => {
+            this.setState({ playlists: featuredListResponse.data.playlists.items })
+          })
+        axios(`${spotify.baseUrl}/categories`, {
+          method: "GET",
+          headers: { 'Authorization': 'Bearer ' + tokenResponse.data.access_token }
+
+        })
+          .then((categoriesResponse) => {
+            this.setState({ categories: categoriesResponse.data.categories.items })
+          })
+      })
+
+  }
   render() {
     const { newReleases, playlists, categories } = this.state;
 
